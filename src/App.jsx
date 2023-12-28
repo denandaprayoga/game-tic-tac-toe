70505043c91a42712810a94b1708217af36af15d
denandaprayoga <denanda79@gmail.com>
2023-12-28 20:49:35 +0700

    membuat square menggunakan dua loop alih-alih mengodekannya

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,25 @@ function Square({ value, onSquareClick }) {
 }
 
 function Board({ xIsNext, squares, onPlay }) {
+  const lengthBoard = 3;
+  const rows = [...Array(lengthBoard).keys()].map((row) => {
+    const columns = [...Array(lengthBoard).keys()].map((col) => {
+      const i = lengthBoard * row + col;
+      return (
+        <Square
+          key={i}
+          value={squares[i]}
+          onSquareClick={() => handleClick(i)}
+        />
+      );
+    });
+    return (
+      <div key={row} className='board'>
+        {columns}
+      </div>
+    );
+  });
+
   function handleClick(i) {
     if (squares[i] || calculateWinner(squares)) {
       return;
@@ -31,17 +50,7 @@ function Board({ xIsNext, squares, onPlay }) {
   return (
     <>
       <div className='status'>{status}</div>
-      <div className='board'>
-        <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
-        <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
-        <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
-        <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
-        <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
-        <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
-        <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
-        <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
-        <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
-      </div>
+      {rows}
     </>
   );
 }
